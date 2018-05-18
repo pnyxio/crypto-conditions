f5f110cb8842be6b9885f2496931827701ca530b
michele <michelegonella@gmail.com>
2018-05-18 09:00:34 +0200

    initial import

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # crypto-conditions
+
+As found at https://github.com/hyperledger/quilt/tree/master/crypto-conditions on 19 March 2018
+
+(see https://github.com/hyperledger/quilt/tree/b0497ff75136a16e32a0336950c88d36e590e39e)
---
+++ src/main/java/org/interledger/cryptoconditions/Sha256Condition.java
@@ -0,0 +1,8 @@
+package org.interledger.cryptoconditions;
+
+/**
+ * Base interface for all non-compound *-SHA-256 conditions that extends {@link SimpleCondition}.
+ */
+public interface Sha256Condition extends SimpleCondition {
+
+}
